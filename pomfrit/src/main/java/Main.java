6e3b42ee3b4044e38193341701f3b21cacd77849
krosengr4 <rosenkev4@gmail.com>
2025-06-09 11:24:58 -0500

    Added notes on dependencies and the sql dependency

--- pomfrit/src/main/java/Main.java
+++ pomfrit/src/main/java/Main.java
@@ -1,8 +1,10 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        //Mapper is a dependency that we need to add in the pom.xml file
         ObjectMapper mapper;
     }
 
