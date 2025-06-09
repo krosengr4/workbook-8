eef649957da4a81d00b5bd996a60750c5d1746de
krosengr4 <rosenkev4@gmail.com>
2025-06-09 11:39:20 -0500

    Added notes on the JDBC programming process and how to talk to the sql database with java code

--- pomfrit/src/main/java/Main.java
+++ pomfrit/src/main/java/Main.java
@@ -3,9 +3,42 @@ import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         //Mapper is a dependency that we need to add in the pom.xml file
         ObjectMapper mapper;
+
+        JDBCConnection();
     }
 
+    public static void JDBCConnection() throws ClassNotFoundException, SQLException {
+        // Load the mySQL driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String username = "root";
+        String password = "611854kr";
+
+        //Use the database URL to point to the correct database
+        Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/sakila", username, password
+        );
+
+        // Create statement used tied to open the connection
+        Statement statement = connection.createStatement();
+
+        //define the query
+        String query = "SELECT * from actor;";
+
+        //execute the query
+        ResultSet results = statement.executeQuery(query);
+
+        // process the results
+        while (results.next()) {
+            String firstName = results.getString("first_name");
+            System.out.println(firstName);
+        }
+
+        //Close the connection
+        connection.close();
+
+    }
 }
