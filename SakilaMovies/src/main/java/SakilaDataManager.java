5d076751b2b3fd171d91509ece0868c077f99e45
krosengr4 <rosenkev4@gmail.com>
2025-06-11 10:53:43 -0500

    Created method in data manager class to search data base and get list of films an actor was in

--- SakilaMovies/src/main/java/SakilaDataManager.java
+++ SakilaMovies/src/main/java/SakilaDataManager.java
@@ -38,4 +38,38 @@ public class SakilaDataManager {
 
         return actorsList;
     }
+
+    public ArrayList<Printable> getFilms(String actorFirstName, String actorLastName) {
+        ArrayList<Printable> filmsList = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection()) {
+
+            String query = "SELECT t1.*, t2.actor_id, t2.first_name, t2.last_name from film t1, actor t2, film_actor t3 " +
+                    "WHERE t1.film_id = t3.film_id " +
+                    "AND t3.actor_id = t2.actor_id " +
+                    "AND first_name = ? AND last_name = ?;";
+            PreparedStatement prepStatement = conn.prepareStatement(query);
+            prepStatement.setString(1, actorFirstName);
+            prepStatement.setString(2, actorLastName);
+
+            ResultSet results = prepStatement.executeQuery();
+
+            while (results.next()) {
+                String title = results.getString("title");
+                String description = results.getString("description");
+                String releaseYear = results.getString("release_year");
+                int length = Integer.parseInt(results.getString("length"));
+                String rating = results.getString("rating");
+
+                Film newFilm = new Film(title, description, releaseYear, length, rating);
+
+                filmsList.add(newFilm);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return filmsList;
+    }
 }
