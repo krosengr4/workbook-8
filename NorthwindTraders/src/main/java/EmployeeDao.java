d9bb4b8a65691bdbd4988ac3d960a95f48593ce8
krosengr4 <rosenkev4@gmail.com>
2025-06-11 14:11:33 -0500

    Created EmployeeDao class to handle getting employee data from the database. Added method to get all employees

---
+++ NorthwindTraders/src/main/java/EmployeeDao.java
@@ -0,0 +1,41 @@
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class EmployeeDao {
+
+    private final DataSource dataSource;
+
+    public EmployeeDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public ArrayList<Printable> getAllEmployees() {
+        ArrayList<Printable> employeesList = new ArrayList<>();
+
+        try (Connection conn = dataSource.getConnection()) {
+
+            String query = "SELECT * FROM employees;";
+            PreparedStatement prepStatement = conn.prepareStatement(query);
+
+            ResultSet results = prepStatement.executeQuery();
+
+            while (results.next()) {
+                String firstName = results.getString("FirstName");
+                String lastName = results.getString("LastName");
+                String title = results.getString("Title");
+
+                Employee newEmployee = new Employee(firstName, lastName, title);
+
+                employeesList.add(newEmployee);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return employeesList;
+    }
+
+}
--- SakilaMovies/src/main/java/SakilaDataManager.java
+++ SakilaMovies/src/main/java/SakilaDataManager.java
@@ -1,8 +1,5 @@
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class SakilaDataManager {
