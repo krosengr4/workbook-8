ee946342118c7a3549dd87a0564ad97b480a848e
krosengr4 <rosenkev4@gmail.com>
2025-06-11 10:56:09 -0500

    Refactored code to use a data manager class. Completed exercise 3 on page 81 of workbook 8

--- SakilaMovies/src/main/java/Main.java
+++ SakilaMovies/src/main/java/Main.java
@@ -11,35 +11,18 @@ public class Main {
     static String password = System.getenv("SQL_PASSWORD");
     static Scanner myScanner = new Scanner(System.in);
 
+    static BasicDataSource dataSource = new BasicDataSource();
+    static SakilaDataManager dataManager = new SakilaDataManager(dataSource);
+
     public static void main(String[] args) {
 
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl(url);
-        dataSource.setUsername(userName);
-        dataSource.setPassword(password);
-
-        SakilaDataManager dataManager = new SakilaDataManager(dataSource);
-
-
+        setDataSource();
         boolean ifContinue = true;
 
         while (ifContinue) {
 
-            System.out.println("Enter the last name of an actor: ");
-            String actorLastName = myScanner.nextLine().trim();
-
-            ArrayList<Printable> actorsList = dataManager.getActors(actorLastName);
-
-            if (actorsList.isEmpty()) {
-                System.out.println("There were no actors with that last name...");
-            } else {
-                printData(actorsList);
-                ifContinue = false;
-
-//                searchForFilm();
-            }
-
-//            searchForActors();
+            searchForActors();
+            searchForFilm();
 
             System.out.println("Would you like to search again? (Y or N): ");
             String userTryAgain = myScanner.nextLine().trim();
@@ -51,6 +34,12 @@ public class Main {
         }
     }
 
+    public static void setDataSource() {
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+    }
+
     public static void searchForActors() {
 
         boolean continueActorSearch = true;
@@ -59,14 +48,14 @@ public class Main {
             System.out.println("Enter the last name of an actor: ");
             String actorLastName = myScanner.nextLine().trim();
 
-            ArrayList<Printable> actorsList = findActors(actorLastName);
+            ArrayList<Printable> actorsList = dataManager.getActors(actorLastName);
 
             if (actorsList.isEmpty()) {
                 System.out.println("There were no actors with that last name...");
             } else {
                 printData(actorsList);
                 continueActorSearch = false;
-                searchForFilm();
+
             }
         }
     }
@@ -80,7 +69,11 @@ public class Main {
             System.out.println("Enter here: ");
             String actorFullName = myScanner.nextLine().trim();
 
-            ArrayList<Printable> filmsList = findFilms(actorFullName);
+            String[] actorNameParts = actorFullName.split(" ");
+            String actorFirstName = actorNameParts[0];
+            String actorLastName = actorNameParts[1];
+
+            ArrayList<Printable> filmsList = dataManager.getFilms(actorFirstName, actorLastName);
 
             if (filmsList.isEmpty()) {
                 System.out.println("There are no films with that actor...");
@@ -96,90 +89,6 @@ public class Main {
                 ifContinue = false;
             }
         }
-    }
-
-    public static ArrayList<Printable> findFilms(String actorFullName) {
-
-        ArrayList<Printable> filmsList = new ArrayList<>();
-        String[] nameParts = actorFullName.split(" ");
-        String firstName = nameParts[0];
-        String lastName = nameParts[1];
-
-        try (BasicDataSource dataSource = new BasicDataSource()) {
-            dataSource.setUrl(url);
-            dataSource.setUsername(userName);
-            dataSource.setPassword(password);
-
-            ResultSet results = getFilmResults(firstName, lastName, dataSource);
-
-            while (results.next()) {
-                String title = results.getString("title");
-                String description = results.getString("description");
-                String releaseYear = results.getString("release_year");
-                int length = Integer.parseInt(results.getString("length"));
-                String rating = results.getString("rating");
-
-                int actorID = Integer.parseInt(results.getString("actor_id"));
-                String resultFirstName = results.getString("first_name");
-                String resultLastName = results.getString("last_name");
-
-                Actor actor = new Actor(actorID, resultFirstName, resultLastName);
-                filmsList.add(actor);
-
-                Film newFilm = new Film(title, description, releaseYear, length, rating);
-                filmsList.add(newFilm);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return filmsList;
-    }
-
-    private static ResultSet getFilmResults(String firstName, String lastName, BasicDataSource dataSource) throws SQLException{
-        Connection connection = dataSource.getConnection();
-
-        String secureQuery = "SELECT t1.*, t2.actor_id, t2.first_name, t2.last_name from film t1, actor t2, film_actor t3 " +
-                "WHERE t1.film_id = t3.film_id " +
-                "AND t3.actor_id = t2.actor_id " +
-                "AND first_name = ? AND last_name = ?;";
-        PreparedStatement preparedStatement = connection.prepareStatement(secureQuery);
-        preparedStatement.setString(1, firstName);
-        preparedStatement.setString(2, lastName);
-
-        return preparedStatement.executeQuery();
-    }
-
-    public static ArrayList<Printable> findActors(String actorLastName) {
-
-        ArrayList<Printable> actorsList = new ArrayList<>();
-
-        try (BasicDataSource dataSource = new BasicDataSource()) {
-            dataSource.setUrl(url);
-            dataSource.setUsername(userName);
-            dataSource.setPassword(password);
-            Connection connection = dataSource.getConnection();
-
-            String secureQuery = "SELECT * FROM actor WHERE last_name = ?;";
-            PreparedStatement preparedStatement = connection.prepareStatement(secureQuery);
-            preparedStatement.setString(1, actorLastName);
-
-            ResultSet results = preparedStatement.executeQuery();
-
-            while (results.next()) {
-                int actorID = Integer.parseInt(results.getString("actor_id"));
-                String firstName = results.getString("first_name");
-                String lastName = results.getString("last_name");
-
-                Actor newActor = new Actor(actorID, firstName, lastName);
-
-                actorsList.add(newActor);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return actorsList;
     }
 
     public static void printData(ArrayList<Printable> dataList) {
