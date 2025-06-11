91601a082592a387ca5759f7a6c1ca4f02471f82
krosengr4 <rosenkev4@gmail.com>
2025-06-11 14:17:22 -0500

    Created method to process getting and displaying all employees

--- NorthwindTraders/src/main/java/Main.java
+++ NorthwindTraders/src/main/java/Main.java
@@ -9,14 +9,11 @@ public class Main {
 
     static Scanner myScanner = new Scanner(System.in);
 
-    static String password = System.getenv("SQL_PASSWORD");
-    static String userName = "root";
-    static String url = "jdbc:mysql://localhost:3306/northwind";
-
     static BasicDataSource dataSource = new BasicDataSource();
     static ProductDao productDao = new ProductDao(dataSource);
     static CustomerDao customerDao = new CustomerDao(dataSource);
     static CategoryDao categoryDao = new CategoryDao(dataSource);
+    static EmployeeDao employeeDao = new EmployeeDao(dataSource);
 
     public static void main(String[] args) {
 
@@ -24,7 +21,6 @@ public class Main {
         boolean ifContinue = true;
 
         while (ifContinue) {
-            String query = "";
 
             System.out.println("-----OPTIONS-----");
             System.out.println("1 - Display All Products\n2 - Display All Customers\n3 - Display All Categories\n4 - Display All Employees\n0 - Exit");
@@ -35,12 +31,10 @@ public class Main {
                 case 1 -> processDisplayAllProducts();
                 case 2 -> processDisplayAllCustomers();
                 case 3 -> processDisplayAllCategories();
-                case 4 -> query = "SELECT * from employees";
+                case 4 -> processAllEmployees();
                 case 0 -> ifContinue = false;
                 default -> System.err.println("ERROR! Please enter a number listed on the screen!");
             }
-
-//            queryNorthwindColumn(userQueryChoice, query);
 
             System.out.println("\n\nWould you like to search for another? (Y or N): ");
             String userContinue = myScanner.nextLine();
@@ -54,6 +48,10 @@ public class Main {
     }
 
     public static void setDataSource() {
+        String password = System.getenv("SQL_PASSWORD");
+        String userName = "root";
+        String url = "jdbc:mysql://localhost:3306/northwind";
+
         dataSource.setUrl(url);
         dataSource.setUsername(userName);
         dataSource.setPassword(password);
@@ -124,58 +122,13 @@ public class Main {
 
     }
 
+    public static void processAllEmployees() {
+        ArrayList<Printable> employeesList = employeeDao.getAllEmployees();
 
-    public static void queryNorthwindColumn(int userChoice, String query) {
-
-        ArrayList<Printable> northwindData = new ArrayList<>();
-
-        try (BasicDataSource dataSource = new BasicDataSource()) {
-
-            dataSource.setUrl(url);
-            dataSource.setUsername(userName);
-            dataSource.setPassword(password);
-
-            Connection connection = dataSource.getConnection();
-
-            Statement statement = connection.createStatement();
-            ResultSet results = statement.executeQuery(query);
-
-            while (results.next()) {
-                if (userChoice == 1) {
-                    int productID = Integer.parseInt(results.getString("ProductID"));
-                    String productName = results.getString("ProductName");
-                    double unitPrice = Double.parseDouble(results.getString("UnitPrice"));
-                    int unitsInStock = Integer.parseInt(results.getString("UnitsInStock"));
-
-                    Product newProduct = new Product(productID, productName, unitPrice, unitsInStock);
-                    northwindData.add(newProduct);
-
-                } else if (userChoice == 2) {
-                    Customer newCustomer = new Customer(results.getString("ContactName"), results.getString("CompanyName"), results.getString("City"),
-                            results.getString("Country"), results.getString("Phone"));
-                    northwindData.add(newCustomer);
-
-                } else if (userChoice == 3) {
-                    int catID = Integer.parseInt(results.getString("CategoryID"));
-                    String catName = results.getString("CategoryName");
-                    String catDescription = results.getString("Description");
-
-                    Category newCategory = new Category(catID, catName, catDescription);
-                    northwindData.add(newCategory);
-
-                } else if (userChoice == 4) {
-                    Employee newEmployee = new Employee(results.getString("FirstName"), results.getString("LastName"), results.getString("Title"));
-                    northwindData.add(newEmployee);
-
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        printData(northwindData);
-        if (userChoice == 3) {
-            getCategoryProducts();
+        if (employeesList.isEmpty()) {
+            System.out.println("There are no employees to display...");
+        } else {
+            printData(employeesList);
         }
     }
 
@@ -188,44 +141,5 @@ public class Main {
                 System.out.println("------------------------------");
             }
         }
-    }
-
-    public static void getCategoryProducts() {
-
-        ArrayList<Printable> productData = new ArrayList<>();
-
-        System.out.println("\n\nSelect a category ID to see its products: ");
-        String userCatChoice = myScanner.nextLine().trim();
-
-        try (BasicDataSource dataSource = new BasicDataSource()) {
-
-            dataSource.setUrl(url);
-            dataSource.setUsername(userName);
-            dataSource.setPassword(password);
-
-            Connection connection = dataSource.getConnection();
-
-            String secureQuery = "SELECT * FROM products WHERE CategoryID = ?";
-            PreparedStatement prepStatement = connection.prepareStatement(secureQuery);
-            prepStatement.setString(1, userCatChoice);
-
-            ResultSet results = prepStatement.executeQuery();
-
-            while (results.next()) {
-
-                int productID = Integer.parseInt(results.getString("ProductID"));
-                String productName = results.getString("ProductName");
-                double unitPrice = Double.parseDouble(results.getString("UnitPrice"));
-                int unitsInStock = Integer.parseInt(results.getString("UnitsInStock"));
-
-                Product newProduct = new Product(productID, productName, unitPrice, unitsInStock);
-                productData.add(newProduct);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        printData(productData);
     }
 }
