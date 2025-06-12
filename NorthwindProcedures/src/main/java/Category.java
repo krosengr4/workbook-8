62575ac45b5fa46329d7e412c563fd94ee0dd116
krosengr4 <rosenkev4@gmail.com>
2025-06-12 00:06:05 -0500

    Created Category class that implements NorthwindData interface

---
+++ NorthwindProcedures/src/main/java/Category.java
@@ -0,0 +1,18 @@
+public class Category implements NorthwindData{
+
+    int categoryID;
+    String name;
+    String description;
+
+    public Category(int categoryID, String name, String description) {
+        this.categoryID = categoryID;
+        this.name = name;
+        this.description = description;
+    }
+
+    public void print() {
+        System.out.println("Category ID: " + this.categoryID);
+        System.out.println("Category Name: " + this.name);
+        System.out.println("Category Description: " + this.description);
+    }
+}
