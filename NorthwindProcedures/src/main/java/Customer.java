83d29389f5a61d222cb1b5517b0aad54ee52a79e
krosengr4 <rosenkev4@gmail.com>
2025-06-12 00:02:32 -0500

    Created customer class and NorthwindData interface

---
+++ NorthwindProcedures/src/main/java/Customer.java
@@ -0,0 +1,25 @@
+public class Customer implements NorthwindData {
+
+    String contactName;
+    String companyName;
+    String city;
+    String country;
+    String phoneNumber;
+
+    public Customer (String contactName, String companyName, String city, String country, String phoneNumber) {
+        this.contactName = contactName;
+        this.companyName = companyName;
+        this.city = city;
+        this.country = country;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void print() {
+        System.out.println("Contact Name: " + this.contactName);
+        System.out.println("Company Name: " + this.companyName);
+        System.out.println("City: " + this.city);
+        System.out.println("Country: " + this.country);
+        System.out.println("Phone Number: " + this.phoneNumber);
+    }
+
+}
